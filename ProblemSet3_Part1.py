f107a577cf064b3e54e581656556ef4b45e9827a
samanthapb01 <slp87@duke.edu>
2024-10-09 12:46:41 -0400

    Task 2

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -1,11 +1,33 @@
+#%%Samantha Pasciullo Boychuck slp87@duke.edu 10/7/2024
+
 #%% Task 1 - Edit code to print as requested
-/*-PS3: Code Block 1--*/
+#/*-PS3: Code Block 1--*/
 
+#Assign names to variables
 mountain = "Denali"
-nickname = 'Mt. McKinley'
-elevation == 20322 
+nickname = "Mt. McKinley"
+elevation = "20322"
 
-print (mountain + ", formerly known as "" + nickname + ",)
-print ()"is " + elevation + '' above sea level.' )
+#Print string
+print(f'{mountain}, sometimes \ncalled "{nickname}", \nis {elevation}\' above sea level.')
 
 #%% Task 2 - Lists and Iteration
+
+#Assign variable to string object
+data_folder = r"W:\859_data\triangle"
+
+#Create a list using specified string objects
+data_list = ["streams.shp", "stream_types.csv", "naip_imagery.tif"]
+
+#Assign a variable to the string
+user_item = "roads.shp"
+
+#Add string object to list
+data_list.append(user_item)
+
+#Loop through list and print file paths
+for item in data_list:
+    full_path = f"{data_folder}\\{item}"
+    print(full_path)
+
+# %% Task 3
