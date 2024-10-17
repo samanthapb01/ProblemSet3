4a102c8b095fb0d5f6506e8c6d579825a3bfea1b
samanthapb01 <slp87@duke.edu>
2024-10-16 22:49:21 -0400

    Task 4 and 5

--- ProblemSet3_Part2.py
+++ ProblemSet3_Part2.py
@@ -36,7 +36,6 @@ vesselDict = {}
 for line in lineList[1:-1]:
     #Split the data into values
     values = line.split(",")
-    print(values)
     #Extract the mmsi value from the list using the mmsi_idx value
     mmsi = values[mmsi_idx]
     #Extract the fleet value
@@ -56,4 +55,60 @@ fleet_value = vesselDict[vesselID]
 #Print statement
 print("Vessel # %s flies the flag of %s" %(vesselID, fleet_value))
 
-# %%
+# %% Task 5.1
+
+#Create a Python file object, i.e., a link to the file's contents
+fileObj2 = open(file='data/raw/loitering_events_20180723.csv',mode='r')
+
+#Read the entire contents into a list object
+lineList2 = fileObj2.read().split("\n")
+
+#Release the link to the file objects (now that we have all its contents)
+fileObj2.close() #Close the file
+
+#Save the contents of the first line in the list of lines to the variable "headerLineString2"
+headerLineString2 = lineList2[0]
+
+#Print the contents of the headerLine
+print(headerLineString2)
+
+# %% Task 5.2
+
+#Split the headerLineString into a list of header items
+headerItems2 = headerLineString2.split(",")
+
+#Store headers to variables
+mmsi2_idx = headerItems2.index("transshipment_mmsi")
+start_lat_idx = headerItems2.index("starting_latitude")
+start_long_idx = headerItems2.index("starting_longitude")
+end_lat_idx = headerItems2.index("ending_latitude")
+end_long_idx = headerItems2.index("ending_longitude")
+
+#Assign variable
+any_meet_criteria = False
+
+#Iterate through all lines (except the header) in the data file:
+for line in lineList2[1:-1]:
+    #Split the data into values
+    values = line.split(",")
+    #Extract the values from the list using the index values
+    mmsi2 = values[mmsi2_idx]
+    start_lat = float(values[start_lat_idx])
+    start_long = float(values[start_long_idx])
+    end_lat = float(values[end_lat_idx])
+    end_long = float(values[end_long_idx])
+    #Examine if vessel crosses the equator and is between longitude of interest
+    equator_cross = (start_lat < 0) and (end_lat > 0)
+    long_placement = (145 < start_long < 155)
+    #Check if both Booleans are true and query dictionary
+    if equator_cross and long_placement:
+        #Set variable to True
+        any_meet_criteria = True
+        #Print statement
+        print("Vessel # %s flies the flag of %s" %(mmsi2, vesselDict[mmsi2]))
+
+#If none True
+if not any_meet_criteria:
+    print("No vessels met criteria")
+
+    
